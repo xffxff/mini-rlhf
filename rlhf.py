ffc496fc2053c0f370ffefab131b929d07d8f21c
xffxff <1247714429@qq.com>
2023-10-03 00:00:23 +0000

    rlhf: training loop

--- rlhf.py
+++ rlhf.py
@@ -180,6 +180,7 @@ device = "cuda"
 
 actor_model = create_hf_model(AutoModelForCausalLM, actor_model_name_or_path, tokenizer)
 actor_optimizer = torch.optim.AdamW(actor_model.parameters(), lr=actor_learning_rate)
+actor_model.optimizer = actor_optimizer
 actor_model.to(device)
 
 ref_model = create_hf_model(AutoModelForCausalLM, actor_model_name_or_path, tokenizer)
@@ -188,6 +189,7 @@ ref_model.to(device)
 critic_model = create_hf_model(AutoModel, critic_model_name_or_path, tokenizer)
 critic_model = RewardModel(critic_model, tokenizer, num_padding_at_beginning)
 critic_optimizer = torch.optim.AdamW(critic_model.parameters(), lr=critic_learning_rate)
+critic_model.optimizer = critic_optimizer
 critic_model.to(device)
 
 reward_model = create_hf_model(AutoModel, critic_model_name_or_path, tokenizer)
@@ -220,6 +222,13 @@ class PPOTrainer:
         self.reward_model = reward_model
         self.tokenizer = tokenizer
         self.max_answer_seq_len = max_answer_seq_len
+
+        self.kl_ctl = 0.1
+        self.clip_reward_value = 5
+        self.cliprange = 0.2
+        self.cliprange_value = 0.2
+        self.gamma = 1.0
+        self.lam = 0.95
 
     def eval(self):
         self.actor_model.eval()
@@ -292,6 +301,115 @@ class PPOTrainer:
             "attention_mask": attention_mask,
         }
 
+    def compute_rewards(
+        self, prompts, log_probs, ref_log_probs, reward_score, action_mask
+    ):
+        kl_divergence_estimate = -self.kl_ctl * (log_probs - ref_log_probs)
+        rewards = kl_divergence_estimate
+        start = prompts.shape[1] - 1
+        ends = start + action_mask[:, start:].sum(1) + 1
+        reward_clip = torch.clamp(
+            reward_score, -self.clip_reward_value, self.clip_reward_value
+        )
+        batch_size = log_probs.shape[0]
+        for j in range(batch_size):
+            rewards[j, start : ends[j]][-1] += reward_clip[j]
+
+        return rewards
+
+    def get_advantages_and_returns(self, values, rewards, start):
+        # Adopted from https://github.com/CarperAI/trlx/blob/main/trlx/models/modeling_ppo.py#L134
+        lastgaelam = 0
+        advantages_reversed = []
+        length = rewards.size()[-1]
+        for t in reversed(range(start, length)):
+            nextvalues = values[:, t + 1] if t < length - 1 else 0.0
+            delta = rewards[:, t] + self.gamma * nextvalues - values[:, t]
+            lastgaelam = delta + self.gamma * self.lam * lastgaelam
+            advantages_reversed.append(lastgaelam)
+        advantages = torch.stack(advantages_reversed[::-1], dim=1)
+        returns = advantages + values[:, start:]
+        return advantages.detach(), returns
+
+    def actor_loss_fn(self, logprobs, old_logprobs, advantages, mask):
+        ## policy gradient loss
+        log_ratio = (logprobs - old_logprobs) * mask
+        ratio = torch.exp(log_ratio)
+        pg_loss1 = -advantages * ratio
+        pg_loss2 = -advantages * torch.clamp(
+            ratio, 1.0 - self.cliprange, 1.0 + self.cliprange
+        )
+        pg_loss = torch.sum(torch.max(pg_loss1, pg_loss2) * mask) / mask.sum()
+        return pg_loss
+
+    def critic_loss_fn(self, values, old_values, returns, mask):
+        ## value loss
+        values_clipped = torch.clamp(
+            values,
+            old_values - self.cliprange_value,
+            old_values + self.cliprange_value,
+        )
+        vf_loss1 = (values - returns) ** 2
+        vf_loss2 = (values_clipped - returns) ** 2
+        vf_loss = 0.5 * torch.sum(torch.max(vf_loss1, vf_loss2) * mask) / mask.sum()
+        return vf_loss
+
+    def train_rlhf(self, inputs):
+        # train the rlhf mode here
+        ### process the old outputs
+        prompts = inputs["prompts"]
+        log_probs = inputs["logprobs"]
+        ref_log_probs = inputs["ref_logprobs"]
+        reward_score = inputs["rewards"]
+        values = inputs["value"]
+        attention_mask = inputs["attention_mask"]
+        seq = inputs["input_ids"]
+
+        start = prompts.size()[-1] - 1
+        action_mask = attention_mask[:, 1:]
+
+        old_values = values
+        with torch.no_grad():
+            old_rewards = self.compute_rewards(
+                prompts, log_probs, ref_log_probs, reward_score, action_mask
+            )
+            ends = start + action_mask[:, start:].sum(1) + 1
+            # we need to zero out the reward and value after the end of the conversation
+            # otherwise the advantage/return will be wrong
+            for i in range(old_rewards.shape[0]):
+                old_rewards[i, ends[i] :] = 0
+                old_values[i, ends[i] :] = 0
+            advantages, returns = self.get_advantages_and_returns(
+                old_values, old_rewards, start
+            )
+
+        ### process the new outputs
+        batch = {"input_ids": seq, "attention_mask": attention_mask}
+        actor_prob = self.actor_model(**batch, use_cache=False).logits
+        actor_log_prob = gather_log_probs(actor_prob[:, :-1, :], seq[:, 1:])
+        actor_loss = self.actor_loss_fn(
+            actor_log_prob[:, start:],
+            log_probs[:, start:],
+            advantages,
+            action_mask[:, start:],
+        )
+        actor_loss.backward()
+
+        self.actor_model.optimizer.step()
+        self.actor_model.optimizer.zero_grad()
+
+        value = self.critic_model.forward_value(
+            **batch, return_value_only=True, use_cache=False
+        )[:, :-1]
+        critic_loss = self.critic_loss_fn(
+            value[:, start:], old_values[:, start:], returns, action_mask[:, start:]
+        )
+        critic_loss.backward()
+        self.critic_model.optimizer.step()
+        self.critic_model.optimizer.zero_grad()
+
+        return actor_loss, critic_loss
+
 
 num_train_epochs = 1
 max_answer_seq_len = 256
@@ -316,4 +434,9 @@ for epoch in range(num_train_epochs):
         out = trainer.generate_experience(
             batch_prompt["prompt"], batch_prompt["prompt_att_mask"]
         )
-        print(out)
+
+        actor_loss, critic_loss = trainer.train_rlhf(out)
+        average_reward = out["rewards"].mean()
+        print(
+            f"actor_loss: {actor_loss}, critic_loss: {critic_loss}, reward: {average_reward}"
+        )
